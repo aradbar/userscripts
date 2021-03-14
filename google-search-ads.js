61f8b30675cbbae04841b2b5a4bf11ed6c8c5273
aradbar <aradbs@gmail.com>
2021-03-14 13:22:32 +0200

    Create google-search-ads.js

---
+++ google-search-ads.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name		 google.com sponsored
+// @include	  /www\.google\.com\/search/
+// ==/UserScript==
+
+function markAds() {
+	var ads = document.querySelectorAll("[data-text-ad='1'] > div");
+
+	Array.prototype.forEach.call(ads, function(ad, i) {
+		var id = 'google-ad-' + i;
+		ad.setAttribute('id', id);
+
+		var adTitlePart = ad.firstChild;
+		adTitlePart.classList.add('ad-part-visible');
+
+		Array.prototype.forEach.call(ad.children, function(adPart, i) {
+			adPart.classList.add('ad-part');
+		});
+
+		var adHeader = adTitlePart.firstChild.children[2];
+
+		// Color the word "Ad"
+		adHeader.firstChild.style.color = 'red';
+
+		// Add show / hide link
+		var showId = id + '-show';
+		adHeader.innerHTML += '&nbsp;&nbsp;<a href="javascript: void(0)" id="' + showId + '">(show)</a>';
+
+		function toggleVisibility() {
+			var adParts = document.querySelectorAll('#' + id + ' .ad-part:not(.ad-part-visible)');
+
+			Array.prototype.forEach.call(adParts, function(adPart, i) {
+				if (adPart.style.display == 'none') {
+					adPart.style.display = '';
+				} else {
+					adPart.style.display = 'none';
+				};
+			});
+
+			if (this.innerHTML == '(show)') {
+				this.innerHTML = '(hide)';
+			} else {
+				this.innerHTML = '(show)';
+			};
+		};
+
+		document.querySelector('#' + showId).onclick = toggleVisibility;
+		toggleVisibility();
+	});
+}
+
+(function() {
+	'use strict';
+	setTimeout(markAds, 1000);
+})();
